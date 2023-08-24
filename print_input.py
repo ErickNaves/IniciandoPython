388f955bc72bd2c2ee1af714f1f913d3e5b8045e
ErickNaves <ericknevesea@gmail.com>
2023-08-24 13:04:31 -0300

    modulo conhecendo a linguagem

---
+++ print_input.py
@@ -0,0 +1,8 @@
+nome = input("Informe o sue nome:")
+idade = input("Informe sua idade:")
+
+
+print(nome, idade)
+print(nome, idade, end=" ... \n")
+print(nome, idade, sep="#", end="... \n")
+print(nome, idade, sep="#")
